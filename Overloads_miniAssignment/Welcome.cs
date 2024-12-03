5ebea3927244a256216eae404be1fbc3ceca226c
OriginalKilljoy <162512500+OriginalKilljoy@users.noreply.github.com>
2024-12-03 14:13:50 +0100

    Assignment done

---
+++ Overloads_miniAssignment/Welcome.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Overloads_miniAssignment
+{
+    internal class Welcome
+    {
+        public void PrintWelcomeMessage()
+        {
+            Console.WriteLine("Hei og velkommen");
+        }
+
+
+        public void PrintWelcomeMessage(string compliment = "Du er snill!")
+        {
+            Console.WriteLine("Hei og velkommen" + compliment);
+        }
+    }
+}
